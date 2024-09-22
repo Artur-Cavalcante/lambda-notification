b60bb7028e5d12559dd7c809ed885e31dfe22966
Artur-Cavalcante <arturcavalcante2014.p@hotmail.com>
2024-09-22 12:47:27 -0300

    adjusting

--- src/notificacao_service.py
+++ src/notificacao_service.py
@@ -2,6 +2,7 @@ import os
 from aws_lambda_powertools import Logger
 
 from src.services.email_service import EmailService
+from src.agendamento_status_enum import AgendamentoStatus
 
 class NotificacaoService():
     def __init__(self, logger: Logger) -> None:
@@ -9,27 +10,33 @@ class NotificacaoService():
         self.email_service = EmailService(self.logger)
     
     def enviar_notificacao(self, agendamento: dict) -> bool:
-        to_emails = [agendamento["email_cliente"]]
-        subject = f"Agendamento {agendamento['id_agendamento']}"
-        
-        if "erro" in agendamento and agendamento["erro"]:
-            body_html = f"""
-                <html>
-                    <body>
-                        <h1>Erro ao realizar agendamento.</h1>
-                        <p>Por favor tente novamente.</p>
-                    </body>
-                </html>
-                """
-        else:
+        to_emails = [agendamento["email_para_envio"]]
+        subject = f"Agendamento {agendamento['id']}"
+
+
+        if agendamento["status_agendamento"] ==  AgendamentoStatus.Confirmado:
             body_html = f"""
                     <html>
                         <body>
                             <h1>Agendamento realizado com sucesso</h1>
-                            <p>Preparando pedido:</p> //TODO colocar horario 
-                            <p>Total pedido: R$ {pedido['total_pedido']}</p>
+                            <p>Horário Agendamento: {agendamento["horario"]}</p> 
+                            <p>CRM Médico: {agendamento["crm_medico"]}</p> 
+                            <p>Paciente: {agendamento["nome_paciente"]}</p> 
                         </body>
                     </html>
                     """
+        
+        if agendamento["status_agendamento"] ==  AgendamentoStatus.Rejeitado:
+            body_html = f"""
+                <html>
+                    <body>
+                        <h1>Agendamento Rejeitado.</h1>
+                        <p>Não foi possível realizar o agendamento, horário não disponível.</p>
+                        <p>Horário Agendamento: {agendamento["horario"]}</p> 
+                        <p>CRM Médico: {agendamento["crm_medico"]}</p> 
+                        <p>Paciente: {agendamento["nome_paciente"]}</p> 
+                    </body>
+                </html>
+                """
 
         self.email_service.enviar_email(to_emails, subject, body_html)
